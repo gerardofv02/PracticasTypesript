e10ee8b312a276caef1888e67484ed6fa33d0e25
jerry <gerardofv02@gmail.com>
2022-10-10 21:39:53 +0200

    Solo algun test por hacer

--- main.ts
+++ main.ts
@@ -1,8 +1,54 @@
-export function add(a: number, b: number): number {
-  return a + b;
+//flat
+//numero maximo de arrays de arrays
+//excepciones
+//strings
+
+const Array1 : any[] = [2,3,4];
+const Array2 : any[]= [2,"13",2];
+const Array3 : any[]= [2,[3,4,[2,5,"1"]],2];
+// creams la funcion para convertir los strings en ints
+function convertToInt(s1 : any[]) {
+  for(let i = 0; i < s1.length; i++){
+    if (typeof(s1[i]) !== 'string'){
+      
+    }
+    else{
+      s1[i] = parseInt(s1[i]);
+    }
+  }
+
+  return s1;
+}
+//creamos la funcion de aplanar
+function aplaneishon(s1: any[]){
+    return s1.flat(Number.MAX_VALUE);
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+function transform(s1 : any[]){
+  return s1.map((n: number, index : number) => {
+    n = 1;
+    for(let i = 0; i < s1.length; i++){
+      if(i !== index){
+        n = n*s1[i];
+      }
+    }
+    return n;
+  })
 }
+
+
+
+function todoJunto(s1 : any[]){
+try{
+let aplanado = aplaneishon(s1);
+console.log(aplanado);
+let semihecho = convertToInt(aplanado);
+console.log(semihecho);
+let hecho = transform(semihecho);
+console.log(hecho);
+}
+catch(Error){
+  console.log(Error);
+}
+}
+let arrayy = todoJunto(Array3);
